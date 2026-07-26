54e0f87b18b0019e747f5ee5e10a02034ae2719e
Andri Wagner <86930766+andriwagner@users.noreply.github.com>
2026-07-26 20:12:15 +0200

    Initial commit

---
+++ src/main/java/com/andriwagner/mc/randomizer/RandomizerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.andriwagner.mc.randomizer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RandomizerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
